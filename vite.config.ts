70bdde04acd769d0d3e234bb6d1756dd25d65732
Ngo Tri <triseungri@gmail.com>
2025-05-22 14:31:05 +0700

    swap tailwind-postcss to tailwind-vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),],
   base: "./",
   css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
   },
 });
