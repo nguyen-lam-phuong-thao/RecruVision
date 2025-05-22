816a38c07343516e96fc8bdf8274f6607a464ad4
Ngo Tri <triseungri@gmail.com>
2025-05-22 11:35:02 +0700

    add depends

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type JSX } from "react";
 import { CallToActionSection } from "./sections/CallToActionSection";
 import { FeaturesSection } from "./sections/FeaturesSection";
 import { FooterSection } from "./sections/FooterSection";
@@ -6,7 +6,7 @@ import { HeroSection } from "./sections/HeroSection";
 import { TestimonialsSection } from "./sections/TestimonialsSection";
 import { WhyChooseSection } from "./sections/WhyChooseSection/WhyChooseSection";
 
-export default function App() {
+export const App = (): JSX.Element => {
   return (
     <div className="flex flex-col w-full bg-white rounded-lg overflow-hidden border-[2.67px] border-solid border-[#ced4da]">
       <main className="w-full">
--- src/pages/Home/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '../../components/navbar/Navbar';
-
-export default function LandingPage() {
-    return (
-        <div className='bg-sky-700'>
-            <Navbar />
-        </div>
-    )
-}
